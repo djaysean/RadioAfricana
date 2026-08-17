1d24145c4cd0878d799e975d54cec7850ad03c30
Oluwatobi Akinwumi <matrixencore@gmail.com>
2026-08-17 15:16:36 +0100

    Fix Jest application smoke test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,6 +4,22 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
+
+jest.mock('../src/navigation', () => {
+  const React = require('react');
+  const {View} = require('react-native');
+
+  return function MockAppNavigator() {
+    return React.createElement(View, {
+      testID: 'mock-app-navigator',
+    });
+  };
+});
+
+jest.mock('../src/services/notifications', () => ({
+  initializeNotifications: jest.fn(() => Promise.resolve(() => {})),
+}));
+
 import App from '../App';
 
 test('renders correctly', async () => {
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+
+  transformIgnorePatterns: [
+    'node_modules/(?!((@react-native|react-native|@react-navigation)/))',
+  ],
+
+  moduleNameMapper: {
+    '^lucide-react-native$':
+      '<rootDir>/node_modules/lucide-react-native/dist/cjs/lucide-react-native.js',
+  },
 };
